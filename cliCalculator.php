bd483ed7069c9df8f6463fd53b03dc88625acd4d
DanaPar <danaprn@gmail.com>
2022-03-18 15:42:15 +0200

    added validation for number inputs

--- cliCalculator.php
+++ cliCalculator.php
@@ -11,11 +11,15 @@ $help = "**Operation menu:**" . "\n" .
     "^ square" . "\n" .
     "V square root" . "\n" .
     "C clear result" . "\n" .
-    "HELP for operation menu" . "\n \n";
+    "HELP for operation menu" . "\n" .
+    "for exit program press CTRL+C" . "\n \n";
 
 echo $help;
 calculator:
 $num1 = readline("Enter number: >>");
+if (!is_numeric($num1)){
+    $num1 = readline("You must type number! Try again: >>");
+}
 option:
 $option = strtoupper(readline("Choose operation: >>"));
 
@@ -28,6 +32,9 @@ if($option === "HELP"){
     $num1 = sqrt($num1);
 }elseif($option === "+" || $option === "-" || $option === "*" || $option === "/"){
     $num2=readline("Enter second number: >>");
+    if (!is_numeric($num2)){
+        $num2 = readline("You must type number! Try again: >>");
+    }
     switch($option){
         case "+":
             $num1 = $num1 + $num2;
@@ -53,5 +60,5 @@ if($option === "HELP"){
     echo $help;
     goto option;
 }
-echo "Result of operation is: $num1" . "\n";
+echo "\n" . "Result of operation is: $num1" . "\n";
 goto option;
